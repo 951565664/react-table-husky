67f8008cc2ee800df3b3ff162d53b9f058e75ba7
小哈husky <951565664@qq.com>
2017-03-22 11:34:45 +0800

    delete indexpage.less

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-import styles from './IndexPage.less';
+
 import HuskyTable from '../components/huskyTable/huskyTable';
 
 function IndexPage() {
